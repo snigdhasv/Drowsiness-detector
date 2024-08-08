9cd034877d62f81d7972d5661f0f8e639556fa20
snigdhasv <snigdhasv1987@gmail.com>
2024-08-08 21:23:32 +0530

    fix bugs

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from imutils import face_utils
 import imutils
 import dlib
 import cv2
-from playsound import playsound  # Import the playsound library
+import pygame
+import time
 
 def eye_aspect_ratio(eye):
     A = distance.euclidean(eye[1], eye[5])
@@ -12,16 +13,24 @@ def eye_aspect_ratio(eye):
     ear = (A + B) / (2.0 * C)
     return ear
 
+# Initialize Pygame mixer for sound playback
+pygame.mixer.init()
+beep_sound = pygame.mixer.Sound('beep.wav')
+
 thresh = 0.25
 frame_check = 20
 detect = dlib.get_frontal_face_detector()
-predict = dlib.shape_predictor(".\shape_predictor_68_face_landmarks.dat")  # Path to your model file
+predict = dlib.shape_predictor(".\\shape_predictor_68_face_landmarks.dat")  # Path to your model file
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
 cap = cv2.VideoCapture(0)
 flag = 0
 alert_triggered = False  # Flag to avoid multiple alerts
+
+def play_beep():
+    beep_sound.play()
+    time.sleep(1)  # Ensure sound plays fully before it can be triggered again
 
 while True:
     ret, frame = cap.read()
@@ -44,7 +53,7 @@ while True:
             flag += 1
             if flag >= frame_check and not alert_triggered:
                 # Play the beep sound
-                playsound('beep.wav')
+                play_beep()
                 cv2.putText(frame, "****************ALERT!****************", (10, 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
                 cv2.putText(frame, "****************ALERT!****************", (10, 325),
